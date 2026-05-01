970c5fab5722586b3abfd4ff8cef853297c066e5
hubert <hubert.kol@poczta.onet.pl>
2026-05-01 17:24:21 +0200

    add description of algorithm in code for generating basic subsets

--- algorithms/backtracking/subsets/basic/main.cpp
+++ algorithms/backtracking/subsets/basic/main.cpp
@@ -3,9 +3,9 @@
 
 namespace basic
 {
-    const std::vector<int> collection = {1, 2, 3};
+    const std::vector<int> collection = {1, 2, 3};  // globalny zbiór niezmienny!
     const int N = collection.size();
-    std::vector<int> current;
+    std::vector<int> current;                       // globalny kontekst (aktualnie budowany subset)
     int cnt;
 
     void printReadySubset()
@@ -18,6 +18,8 @@ namespace basic
 
     void genSubset(int index)
     {
+        // WARUNEK STOPU: rozpatrzyliśmy wszystkie elementy
+        // current zawiera gotowy subset – wypisujemy go
         if (index == N)
         {
             cnt++;
@@ -25,11 +27,13 @@ namespace basic
             return;
         }
 
+        // DECYZJA 1: bierzemy element collection[index] do subsetu
         current.push_back(collection[index]);
-        genSubset(index + 1);
-        current.pop_back();
+        genSubset(index + 1);   // idziemy głębiej z tym elementem
+        current.pop_back();     // cofamy – usuwamy element
 
-
+        // DECYZJA 2: pomijamy element collection[index]
+        // (nie dodajemy nic do current)
         genSubset(index + 1);
     }
 }
@@ -37,7 +41,7 @@ namespace basic
 
 int main()
 {
-    basic::genSubset(0);
+    basic::genSubset(0);    // zaczynamy od pierwszego elementu (index 0)
     std::cout << "Number of subsets: " << basic::cnt << std::endl;
     return 0;
 }
