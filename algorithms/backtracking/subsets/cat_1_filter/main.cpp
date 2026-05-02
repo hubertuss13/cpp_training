a5a02dae74fd6c4649ed3d80b06c6e0be5195c68
hubert <hubert.kol@poczta.onet.pl>
2026-05-02 14:58:14 +0200

    subset generation: add implementation for subsets with specified K length only

--- algorithms/backtracking/subsets/cat_1_filter/main.cpp
+++ algorithms/backtracking/subsets/cat_1_filter/main.cpp
@@ -68,8 +68,68 @@ namespace target_sum
     }
 }
 
+namespace target_length
+{
+    const std::vector<int> collection = {1, 2, 3, 4};
+    const int N = collection.size();
+    const int K = 2;    // szukamy subsetów o długości dokładnie K
+
+    std::vector<int> current;
+    int cnt;
+
+
+    void printTargetSubset()
+    {
+        cnt++;
+        std::cout << "[ ";
+        for (int x : current)
+            std::cout << x << " ";
+        std::cout << "]\n";
+    }
+    // Przypadek A: mamy już w current za dużo elementów – więcej niż K. Czy ma sens schodzić głębiej?
+    // Przypadek B: mamy już dokładnie K elementów w current, ale index != N. Czy ma sens schodzić głębiej?
+    // Przypadek C (trudniejszy): zostało nam do rozpatrzenia bardzo mało elementów – 
+    //             tak mało, że nawet gdybyśmy wzięli wszystkie pozostałe, nadal nie dobilibyśmy do K elementów. Czy ma sens schodzić głębiej?
+    void genSubset(int index)
+    {
+        // PRUNING A: mamy już za dużo elementów – ta gałąź nie ma sensu
+        // USUNIĘTY->zakomentowany, bo nigdy nie był osiągany w tej implementacji
+        // tzn. Warunek stopu działa jak "sufit" – gdy current.size() == K zawsze wracamy, więc current.size() nigdy nie ma szansy przekroczyć K
+        if (static_cast<int>(current.size()) > K) return;
+
+        // PRUNING C: zostało za mało elementów żeby dobić do K
+        if ((N - index) < (K - static_cast<int>(current.size()))) return;
+
+        // WARUNEK STOPU B: mamy dokładnie K elementów – znaleźliśmy subset
+        if (static_cast<int>(current.size()) == K)
+        {
+            printTargetSubset();
+            return;
+        }
+
+        // DECYZJA 1: bierzemy collection[index]
+        current.push_back(collection[index]);
+        genSubset(index + 1);   // idz głębiej
+        current.pop_back();     // powrót-cofamy
+
+        // DECYZJA 2: pomijamy collection[index]
+        genSubset(index + 1);
+    }
+
+    void test_gen_subset_with_given_K_length()
+    {
+        std::cout << "Initial set: [ ";
+        for (int i : target_length::collection) std::cout << i << " ";
+        std::cout << "]\n";
+        std::cout << "Subsets with target length equal to " << K << ":\n";
+        target_length::genSubset(0);
+        std::cout << "Cnt: " << target_length::cnt << std::endl;
+    }
+}
+
 int main()
 {
     target_sum::test_gen_subset_with_target_sum_equal_to_target();
+    target_length::test_gen_subset_with_given_K_length();
     return 0;
 }
