6e782eaed3c9b00f9a8e90332ce811f37a293723
hubert <hubert.kol@poczta.onet.pl>
2026-05-10 13:42:17 +0200

    Counting subsets: add implementation for counting subsets with sum equal to TARGET sum

--- algorithms/backtracking/subsets/cat_3_counting_subsets/main.cpp
+++ algorithms/backtracking/subsets/cat_3_counting_subsets/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 #include <vector>
 
+namespace counting_subsets_with_sum_equal_to_target
+{
+    const std::vector<int> collection = {1, 2, 3, 4};
+    const int N = collection.size();
+    const int TARGET = 5;
+
+    int cnt = 0;
+    int currentSum = 0;
+
+    void genSubset(int index)
+    {
+        if (currentSum > TARGET) return;
+
+        if (index == N)
+        {
+            if (currentSum == TARGET)
+            {
+                cnt++;
+            }
+            return;
+        }
+
+        currentSum += collection[index];
+        genSubset(index + 1);
+        currentSum -= collection[index];
+
+
+        genSubset(index + 1);
+    }
+
+    void test_subset_count_with_target_sum()
+    {
+        genSubset(0);
+        std::cout << "Original subset: [ ";
+        for (auto x : collection)
+            std::cout << x << " ";
+        std::cout << "]\n";
+        std::cout << "Number of subsets with TARGET sum = " << TARGET << " is: " << cnt << std::endl;
+    }
+}
 
 int main()
 {
+    counting_subsets_with_sum_equal_to_target::test_subset_count_with_target_sum();
 
     return 0;
 }
