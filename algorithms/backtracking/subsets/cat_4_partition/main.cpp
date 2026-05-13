1f8d72365331b2bf801bfd3a37d0bd639713a12f
Hubert Kolbuc <hubert@Katarzynas-MacBook-Air.local>
2026-05-13 22:17:04 +0200

    gen subset: add implementation for balanced partition

--- algorithms/backtracking/subsets/cat_4_partition/main.cpp
+++ algorithms/backtracking/subsets/cat_4_partition/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <climits> 
 
 
 // Zadania - Kategoria 4 – Podział zbioru (partition problem)
@@ -71,14 +72,13 @@ namespace equal_partition
 
         if (TOTAL % 2 != 0)
         {
-            std::cout << "Cannot partition – total is odd\n";
+            std::cout << "Cannot partition - total is odd\n";
             return;
-    }
+        }
 
         genSubset(0);
         if (found == false)
             std::cout << "Cannot partiotion the original set\n";
-
     }
 
 }   // namespace equal_partition
@@ -89,12 +89,65 @@ namespace balanced_partition
     // Cel: Podziel zbior collection na dwie grupy tak żeby różnica ich sum była jak najmniejsza
 
     const std::vector<int> collection = {3, 1, 4, 2, 5};
+    const int N = collection.size();
+    const int TOTAL = std::accumulate(collection.begin(), collection.end(), 0);
+
+    std::vector<int> current;
+    int currentSum = 0;
+
+    std::vector<int> bestSubset;
+    int bestSum = INT_MIN;
+
+    void genSubset(int index)
+    {
+        if (N == index)
+        {
+            if (abs((TOTAL / 2) - currentSum) < abs((TOTAL / 2) - bestSum))
+            {
+                bestSum = currentSum;
+                bestSubset = current;
+            }
+            return;
+        }
+
+        current.push_back(index);
+        currentSum += collection[index];
+        genSubset(index + 1);
+        current.pop_back();
+        currentSum -= collection[index];
+
+        genSubset(index + 1);
+    }
+
+    void test_balanced_partition()
+    {
+        std::cout << "Original set: [ ";
+        for (auto x : collection)
+            std::cout << x << " ";
+        std::cout << "]\n";
+
+        genSubset(0);
+
+        std::cout << "Balanced Partition with TOTAL / 2 = " << (TOTAL / 2) << "\n" << "First subset: [ ";
+        for (auto x : bestSubset)
+            std::cout << collection[x] << " ";
+        std::cout << "]\n";
+
+        std::cout << "Second subset: [ ";
+        for (int i = 0; i < N; ++i)
+        {
+            if (std::find(bestSubset.begin(), bestSubset.end(), i) == bestSubset.end())
+                std::cout << collection[i] << " ";
+        }
+        std::cout << "]\n";
+    }
 
 }   // namespace balanced_partition
 
 
 int main()
 {
-    equal_partition::test_partition();
+    //equal_partition::test_partition();
+    balanced_partition::test_balanced_partition();
     return 0;
 }
