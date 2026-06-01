ae3ca2e7b2b403011451faf32200b8614e2ce176
Hubert Kolbuc <hubert@Katarzynas-MacBook-Air.local>
2026-06-01 22:14:31 +0200

    add function to printArray

--- algorithms/mergeSort/main.cpp
+++ algorithms/mergeSort/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <vector>
 
+void printArr(const std::vector<int> & arr, std::string label = "Array = ")
+{
+    std::cout << label << std::endl;
+    std::cout << "[ ";
+    for (auto i : arr)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "]" << std::endl;
+}
 
 int main()
 {
+    std::vector<int> arr = {39, 1, 14, 4, 7, 3, 10, 0, 19};
+    printArr(arr, "Array before sort:");
+
     return 0;
 }
