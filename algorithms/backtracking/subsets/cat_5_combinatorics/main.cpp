adb1c9eee6c98c6f5fe61378a4e7a78e8314b57f
Hubert Kolbuc <hubert@Katarzynas-MacBook-Air.local>
2026-06-04 12:53:43 +0200

    add partly-finished solution for unique subsets from duplicated set.TODO: add sorting set before backtracking

--- algorithms/backtracking/subsets/cat_5_combinatorics/main.cpp
+++ algorithms/backtracking/subsets/cat_5_combinatorics/main.cpp
@@ -51,7 +51,7 @@ namespace k_element_combination
     {
         std::cout << "Test k= " << K << " element combination from set: [1, 2, 3, 4]\n";
         genSubset(0);
-        std::cout << "CNT:" << cnt << std::endl;
+        std::cout << "CNT:" << cnt << std::endl << std::endl;
     }
 
 } // namespace k_element_combination
@@ -93,7 +93,7 @@ namespace powerset
     {
         std::cout << "Test powerset of original set: [1, 2, 3, 4]\n";
         genSubset(0);
-        std::cout << "CNT=" << cnt << std::endl;
+        std::cout << "CNT=" << cnt << std::endl << std::endl;
     }
 
 }  // namespace powerset
@@ -103,6 +103,54 @@ namespace subsets_with_no_duplicates
 {
     // Cel: Ze zbioru collection zawierajacego powtorzenia wygeneruj wszystkie unikalne subsety
 
+    // Zasada: Jeśli NUMS[index] == NUMS[index-1] i poprzedni element został pominięty – wzięcie bieżącego elementu da duplikat. Odcinamy gałąź wzięcia.
+    // Ścieżka A: BIERZEMY  pierwsze 2, pomijamy drugie 2 → { 1, 2 }
+    // Ścieżka B: POMIJAMY  pierwsze 2, BIERZEMY drugie 2 → { 1, 2 }  ← duplikat!
+
+    // W kodzie potrzebujesz wiedzieć czy poprzedni element był wzięty czy pominięty. Jak to śledzić? Masz dwie opcje:
+    // Opcja A: dodać parametr bool prevTaken do funkcji rekurencyjnej.
+    // Opcja B: użyć wektora used[] podobnie jak przy permutacjach.
+
+    std::vector<int> collection = {1, 2, 2};
+    const int N = collection.size();
+    int cnt = 0;
+    std::vector<int> current;
+
+    void printSubset()
+    {
+        std::cout << "[ ";
+        for (int x : current)
+            std::cout << x << " ";
+        std::cout << "]\n";
+    }
+
+    void genSubset(int index, bool prevTaken)
+    {
+        if (index == N)
+        {
+            cnt++;
+            printSubset();
+            return;
+        }
+
+        if (index == 0 || collection[index] != collection[index - 1] || prevTaken == true)
+        {
+            // DECYZJA 1: bierzemy collection[index] – ale tylko gdy nie jest duplikatem pominiętego
+            current.push_back(collection[index]);
+            genSubset(index + 1, true);
+            current.pop_back();
+        }
+
+        // DECYZJA 2: pomijamy collection[index] – zawsze legalne
+        genSubset(index + 1, false);
+    }
+
+    void test_no_duplicated_subsets()
+    {
+        std::cout << "Test unique subsets from original duplicated set: [1, 2, 2]\n";
+        genSubset(0, false);
+        std::cout << "CNT: " << cnt << std::endl << std::endl;
+    }
 
 } // namespace subsets_with_no_duplicates
 
@@ -110,5 +158,6 @@ int main()
 {
     k_element_combination::test_combination();
     powerset::test_powerset();
+    subsets_with_no_duplicates::test_no_duplicated_subsets();
     return 0;
 }
