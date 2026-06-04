d32ae00532f42b9b1a47eb5c4c02f16956dce77d
Hubert Kolbuc <hubert@Katarzynas-MacBook-Air.local>
2026-06-04 11:54:56 +0200

    add 5th category to the template

---
+++ algorithms/backtracking/subsets/cat_5_combinatorics/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+
+// Zadania - Kategoria 5 – Zadania kombinatoryczne
+// 1. Kombinacje k elementów z n - wygeneruj wszystkie subsety dokładnie k-elementowe
+// 2. Power set – wygeneruj absolutnie wszystkie subsety
+// 3. Subsety bez duplikatów – gdy zbiór wejściowy zawiera powtórzenia
+
+namespace k_element_combination
+{
+    // Cel: Wygeneruj wszystkie możliwe kombinacje k elementów z podanego zbioru collection
+
+    const std::vector<int> collection = {1, 2, 3, 4};
+    const int N = collection.size();
+    const int K = 2;
+
+    std::vector<int> current;
+} // namespace k_element_combination
+
+
+namespace powerset
+{
+    // Cel: Wygeneruj wszystkie możliwe subsety ze zbioru collection
+
+
+}  // namespace powerset
+
+
+namespace subsets_with_no_duplicates
+{
+    // Cel: Ze zbioru collection zawierajacego powtorzenia wygeneruj wszystkie unikalne subsety
+    
+
+} // namespace subsets_with_no_duplicates
+
+int main()
+{
+
+    return 0;
+}
